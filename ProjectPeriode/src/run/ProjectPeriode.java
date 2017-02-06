348a4bac8eb3d67ff92f7646c6dd0b0aac076b0f
Michiel Tombergen <michielhttombergen@gmail.com>
2017-02-06 08:39:19 +0100

    right

--- ProjectPeriode/src/run/ProjectPeriode.java
+++ ProjectPeriode/src/run/ProjectPeriode.java
@@ -1,28 +1,10 @@
-
 package run;
 
 import model.Model;
 
 public class ProjectPeriode {
-
-	public static Model model;
-	
 	public static void main(String[] args) {
-		model = new Model();
-	}
-	
-	public static void go() {
+		Model model = new Model();
 		model.run();
-	}
-	
-	public static void step() {
-		model.tick();
-	}
-	
-	public static void hstep(){
-		for(int t = 0; t < 100; t++){
-			model.tick();
-		}
-	}
+	}	
 }
-
