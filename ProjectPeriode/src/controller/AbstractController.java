6912f52f09e0c331152211e7f97e1b2ffcf3201d
Michiel Tombergen <michielhttombergen@gmail.com>
2017-02-06 08:46:00 +0100

    comments op de abstracte controller.

--- ProjectPeriode/src/controller/AbstractController.java
+++ ProjectPeriode/src/controller/AbstractController.java
@@ -2,6 +2,13 @@ package controller;
 
 import model.*;
 
+/**
+ * De AbstractController klasse doet weinig op dit moment van het project. Hij zegt alleen dat 
+ * het een protected sub is van Model en er wordt een AbstractController aangemaakt met het 
+ * object model.
+ * @author Michiel
+ *
+ */
 public abstract class AbstractController{
 	protected Model model;
 	
