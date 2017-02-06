d9a81c0c5a82fe1ca27f6bad5154a3217d5839fc
Michiel Tombergen <michielhttombergen@gmail.com>
2017-02-06 08:40:11 +0100

    is goed

--- ProjectPeriode/src/run/ProjectPeriode.java
+++ ProjectPeriode/src/run/ProjectPeriode.java
@@ -1,10 +1,28 @@
+
 package run;
 
 import model.Model;
 
 public class ProjectPeriode {
+
+	public static Model model;
+	
 	public static void main(String[] args) {
-		Model model = new Model();
+		model = new Model();
+	}
+	
+	public static void go() {
 		model.run();
-	}	
+	}
+	
+	public static void step() {
+		model.tick();
+	}
+	
+	public static void hstep(){
+		for(int t = 0; t < 100; t++){
+			model.tick();
+		}
+	}
 }
+
